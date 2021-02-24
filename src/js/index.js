1412b48926b0b6a7e5aa554acac33e92341846be
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-02-24 15:35:18 +0600

    Added All Files

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,32 @@
+// Sticky Nav
+const navbar = document.querySelector('#header-section nav');
+const navbarLogo = document.querySelector('.navbar-brand img');
+window.onscroll = function () {
+    stickyNav();
+};
+function stickyNav() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        navbar.classList.add('sticky-nav');
+        navbarLogo.src = 'img/black-logo.png';
+    } else {
+        navbar.classList.remove('sticky-nav');
+        navbarLogo.src = 'img/logo.png';
+    }
+}
+
+// Mobile Menu Icon Animation
+const navbarToggler = document.querySelector('.navbar-toggler');
+navbarToggler.addEventListener('click', () => {
+    navbarToggler.classList.toggle('toggle');
+});
+
+// Magnific PopUp
+$('.banner-pop-up-vidoe').magnificPopup({
+    type: 'iframe',
+});
 
 // Testimonial Slider
-var swiper = new Swiper('.swiper-container.testimonial-slider', {
+const swiper = new Swiper('.swiper-container.testimonial-slider', {
     effect: 'coverflow',
     initialSlide: 2,
     grabCursor: true,
@@ -45,25 +71,22 @@ var swiper = new Swiper('.swiper-container.testimonial-slider', {
     },
 });
 
-
 // Portfolios Filtering Layout
-var $grid = $('.portfolios').isotope({
+const $grid = $('.portfolios').isotope({
     itemSelector: '.portfolio-item',
     percentPosition: true,
     masonry: {
-      // use outer width of grid-sizer for columnWidth
-      columnWidth: 1
-    }
-  })
-
-  // Filter Portfolio items on button click
-  $('.portfolio-category-buttons').on( 'click', 'button', function() {
-    var filterValue = $(this).attr('data-filter');
+        // use outer width of grid-sizer for columnWidth
+        columnWidth: 1,
+    },
+});
+// Filter Portfolio items on button click
+$('.portfolio-category-buttons').on('click', 'button', function () {
+    const filterValue = $(this).attr('data-filter');
     $grid.isotope({ filter: filterValue });
-  });
-
+});
 //   Show Active Class Button Onclick
-  $(".portfolio-category-buttons button").click(function () {
-    $(".portfolio-category-buttons button").removeClass("is-clicked");
-    $(this).addClass("is-clicked");
-  });
+$('.portfolio-category-buttons button').click(function () {
+    $('.portfolio-category-buttons button').removeClass('is-clicked');
+    $(this).addClass('is-clicked');
+});
